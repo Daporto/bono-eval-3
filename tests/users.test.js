9e430b6abd3f7dc807233c7923d283288c4dbbf4
Daniel Porto <daporto@uninorte.edu.co>
2021-09-30 17:59:12 +0000

    users test description was changed

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('GET /user', () => {
-  it('Should response hello world', async () => {
+describe('GET /users', () => {
+  it('Should response My users route', async () => {
     const response = await request(app).get('/users');
     expect(response.body.data).toBe('My users route');
   });
