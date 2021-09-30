5c008418719bf8e36befa04c1a451af0b7d53bc1
Daniel Porto <daporto@uninorte.edu.co>
2021-09-30 18:30:43 +0000

    feat: jest.config.js file was created

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./src/routes/index');
@@ -20,7 +19,7 @@ const myCustomMiddleware = (req, res, next) => {
   next();
 };
 
-app.use('/', myCustomMiddleware, indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use((req, res, next) => {
